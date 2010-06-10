7524cd12f5b96898d073fb65209f918025c97ece
Harri Mähönen <ext-harri.mahonen@nokia.com>
2010-06-10 14:59:49 +0300

    session: manage and track session bus.
    use inotify to track the session bus address file and changes to it.
    when bus changes, reconnect the tone mapper and profile.
    
    Signed-off-by: Janne Mäntyharju <ext-janne.1.mantyharju@nokia.com>

---
+++ src/session.h
@@ -0,0 +1,25 @@
+/*
+ * ngfd - Non-graphical feedback daemon
+ *
+ * Copyright (C) 2010 Nokia Corporation. All rights reserved.
+ *
+ * Contact: Xun Chen <xun.chen@nokia.com>
+ *
+ * This software, including documentation, is protected by copyright
+ * controlled by Nokia Corporation. All rights are reserved.
+ * Copying, including reproducing, storing, adapting or translating,
+ * any or all of this material requires the prior written consent of
+ * Nokia Corporation. This material also contains confidential
+ * information which may not be disclosed to others without the prior
+ * written consent of Nokia.
+ */
+
+#ifndef SESSION_H
+#define SESSION_H
+
+#include "context.h"
+
+int  session_create  (Context *context);
+void session_destroy (Context *context);
+
+#endif /* SESSION_H */
