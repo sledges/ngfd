418dbccb42fd913b7ec82bcef00279ea92ac4dec
Harri Mähönen <ext-harri.mahonen@nokia.com>
2010-07-12 10:33:17 +0300

    license change to LGPL.
    Signed-off-by: Harri Mähönen <ext-harri.mahonen@nokia.com>

--- src/main.c
+++ src/main.c
@@ -1,17 +1,22 @@
 /*
- * ngfd - Non-graphical feedback daemon
+ * ngfd - Non-graphic feedback daemon
  *
- * Copyright (C) 2010 Nokia Corporation. All rights reserved.
- *
+ * Copyright (C) 2010 Nokia Corporation.
  * Contact: Xun Chen <xun.chen@nokia.com>
  *
- * This software, including documentation, is protected by copyright
- * controlled by Nokia Corporation. All rights are reserved.
- * Copying, including reproducing, storing, adapting or translating,
- * any or all of this material requires the prior written consent of
- * Nokia Corporation. This material also contains confidential
- * information which may not be disclosed to others without the prior
- * written consent of Nokia.
+ * This work is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <glib.h>
